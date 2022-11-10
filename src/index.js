4815ff4f02adcf97c795fd7116154ebc221f5d57
alexanderhetzel <alexander.hetzel@htl-wels.at>
2022-11-10 23:59:42 +0100

    first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Header() {
+    return (
+        <header>
+            <nav>
+                <img src="./logo512.png" width="40px" alt=""/>
+            </nav>
+        </header>
+    )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Navbar() {
+    return (
+        <div>
+            <h1>React</h1>
+            <ol>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ol>
+        </div>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+            <small>@ 2022 Hetzel Development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+function Page() {
+    return (
+        <div>
+            <Header/>
+            <Navbar/>
+            <Footer/>
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Page/>)
